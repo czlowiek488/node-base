f9effb76b5d5766381fbaf810fd9118fb0db77ed
LukaszKlejszta <LukaszKlejszta@gmail.com>
2019-09-13 18:28:38 +0200

    + core/time

---
+++ core/time.js
@@ -0,0 +1,5 @@
+exports.moment = require('./moment');
+
+exports.now = () => this.moment().format('YY/MM/DD hh:mm:ss');
+exports.ms = () => this.moment().valueOf();
+exports.nano = () => process.hrtime().join('');
