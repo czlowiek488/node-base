958e0215f4560d64c41754b840451f26e4db4942
LukaszKlejszta <LukaszKlejszta@gmail.com>
2019-09-16 14:45:46 +0200

    fixup! + core/kv-store

--- core/kv-store.js
+++ core/kv-store.js
@@ -1,7 +1,7 @@
 module.exports = (store = new Map()) => {
-    if(store['get'] === undefined 
-        || store['set'] === undefined
-        || store['delete'] === undefined) throw Error('KV-STORE INITIALIZATION FAILED!');
+    if(!(store['get'] instanceof Funtion && store['set'] instanceof Funtion && store['delete'] instanceof Funtion)) {
+        throw Error(`KV-STORE INITIALIZATION FAILED! - ${JSON.stringify({get: store.get, set: store.set, delete: store.delete, setex: store.setex})}`);
+    }
     const fake_setex = (key, value, time) => {
         store.set(key, value);
         setTimeout(() => store.delete(key), time);
@@ -11,9 +11,9 @@ module.exports = (store = new Map()) => {
         set(key, value) { return store.set(key, value) },
         delete(key) { return store.delete(key, value) },
         setex(key, value, time) { 
-            return store[setex] === undefined
-                ? fake_setex(key, value, time)
-                : store.setex(key, value, time)
+            return store[setex] instanceof Funtion
+                ? store.setex(key, value, time)
+                : fake_setex(key, value, time)
         },
     };
     return Object.freeze(accessable);
