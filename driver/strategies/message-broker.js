04238f78ae63cc1e24df1e1f6294c26ca42e9109
LukaszKlejszta <LukaszKlejszta@gmail.com>
2019-09-18 01:12:39 +0200

    logger refactored

--- driver/strategies/message-broker.js
+++ driver/strategies/message-broker.js
@@ -8,12 +8,12 @@ module.exports = (broker, { json = false, logging = false } = { json: false, log
     }
     const fromJSON = string => !!json ? JSON.parse(string) : string;
     const toJSON = obj => !!json ? JSON.stringify(obj) : obj;
-    const log = (...args) => logging === true ? logger.driver(...args) : null;
+    const log = (...args) => logging === true ? logger.driver('MessageBroker', ...args) : null;
     const accessable = {
-        subscribe(channel_id, callback, ...rest) { return log('sub', channel_id) || broker.subscribe(channel_id, (message, ...callback_rest) => callback(fromJSON(message), ...callback_rest), ...rest) },
-        unsubscribe(channel_id, ...rest) { return log('unsub', channel_id) || broker.unsubscribe(channel_id, ...rest) },
-        publish(channel_id, message, ...rest) { return log('pub', channel_id) || broker.publish(channel_id, toJSON(message), ...rest) },
-        request(channel_id, message, ...rest) { return log('req', channel_id) || broker.request(channel_id, toJSON(message), ...rest).then(fromJSON) },
+        subscribe(channel_id, callback, ...rest) { return log('sub', { channel_id }) || broker.subscribe(channel_id, (message, ...callback_rest) => callback(fromJSON(message), ...callback_rest), ...rest) },
+        unsubscribe(channel_id, ...rest) { return log('unsub', { channel_id }) || broker.unsubscribe(channel_id, ...rest) },
+        publish(channel_id, message, ...rest) { return log('pub', { channel_id }) || broker.publish(channel_id, toJSON(message), ...rest) },
+        request(channel_id, message, ...rest) { return log('req', { channel_id }) || broker.request(channel_id, toJSON(message), ...rest).then(fromJSON) },
     };
     return Object.freeze(accessable);
 };
